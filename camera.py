f54cec18ae6b33a3ea2431a36ad5ba21fadfc005
hmsgobrr <metalcorehms@gmail.com>
2024-08-10 17:20:48 +0700

    midas integration

--- camera.py
+++ camera.py
@@ -3,21 +3,28 @@ import numpy as np
 from threading import Thread, Lock
 
 class VideoStream:
-    def __init__(self, camres=(540,270), detres=(320, 320), framerate=36, lcamidx=0, rcamidx=1):
+    def __init__(self, camres=(540,270), detres=(320, 320), framerate=36, lcamidx=0, rcamidx=2):
         self.detres = detres
 
-        self.lcam = cv2.VideoCapture(lcamidx)
+        self.lcam = cv2.VideoCapture(lcamidx, cv2.CAP_V4L2)
         self.lcam.set(cv2.CAP_PROP_FRAME_WIDTH, camres[0])
         self.lcam.set(cv2.CAP_PROP_FRAME_HEIGHT, camres[1])
         self.lcam.set(cv2.CAP_PROP_FPS, framerate)
 
-        self.rcam = cv2.VideoCapture(rcamidx)
+        self.rcam = cv2.VideoCapture(rcamidx, cv2.CAP_V4L2)
         self.rcam.set(cv2.CAP_PROP_FRAME_WIDTH, camres[0])
         self.rcam.set(cv2.CAP_PROP_FRAME_HEIGHT, camres[1])
         self.rcam.set(cv2.CAP_PROP_FPS, framerate)
 
-        self.lframe = None
-        self.rframe = None
+        ret, read = self.lcam.read()
+        if not ret:
+            print("HEYA NO FREM FROM LEFT CAM")
+        self.lframe = cv2.resize(read, (self.detres[0], int(self.detres[1]/2)), interpolation=cv2.INTER_NEAREST)
+
+        ret, read = self.rcam.read()
+        if not ret:
+            print("HEYA NO FREM FROM RIGHT CAM")
+        self.rframe = cv2.resize(read, (self.detres[0], int(self.detres[1]/2)), interpolation=cv2.INTER_NEAREST)
 
         self.stopped = False
 
@@ -28,35 +35,28 @@ class VideoStream:
         Thread(target=self.update, args=(self.rcam, False)).start()
         return self
 
-    def getframe(self, cam, isleft):
-        ret, read = cam.read()
-        if not ret:
-            camstr = "left" if isleft else "right"
-            print(f"[ERROR] Failed to read from {camstr} camera")
-            exit()
-        return cv2.resize(read, (self.detres[0], int(self.detres[1]/2)), interpolation=cv2.INTER_NEAREST)
-
     def update(self, cam, isleft):
         while True:
             if self.stopped:
                 cam.release()
                 return
-            
+
+            ret, read = cam.read()
+            if not ret:
+                camstr = "left" if isleft else "right"
+                print(f"[ERROR] Failed to read from {camstr} camera")
+                return
+            frame = cv2.resize(read, (self.detres[0], int(self.detres[1]/2)), interpolation=cv2.INTER_NEAREST)
+
             with self.lock:
                 if isleft:
-                    self.lframe = self.getframe(self.lcam, True)
+                    self.lframe = frame
                 else:
-                    self.rframe = self.getframe(self.rcam, False)
+                    self.rframe = frame
 
     def read(self):
         with self.lock:
-            if self.lframe is None:
-                self.lframe = self.getframe(self.lcam, True)
-            if self.rframe is None:
-                self.rframe = self.getframe(self.rcam, False)
-            stacked = np.vstack((self.lframe, self.rframe))
-            cv2.imwrite("outputs/ast.jpg", stacked)
-            return stacked
+            return np.vstack((self.lframe, self.rframe))
 
     def stop(self):
         self.stopped = True
--- detect.py
+++ detect.py
@@ -1,18 +1,15 @@
 import cv2
 import time
-import argparse
+import torchvision.transforms as T
+import numpy as np
+import onnxruntime as ort
 from camera import VideoStream
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--li", help="Left camera index", default=0)
-parser.add_argument("--ri", help="Right camera index", default=1)
-args = parser.parse_args()
 
 # Load COCO labels
 with open('coco.names') as f:
     labels = f.read().rstrip('\n').split('\n')
 
-# Load the model
+# Init Object Detection model
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 weightsPath = 'frozen_inference_graph.pb'
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
@@ -21,7 +18,21 @@ net.setInputScale(1.0 / 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-stream = VideoStream(lcamidx=int(args.li), rcamidx=int(args.ri)).start()
+# Init MiDas model
+midas_model_path = "model-small.onnx"
+ort_session = ort.InferenceSession(midas_model_path)
+def transform_image(image):
+    transform = T.Compose([
+        T.ToTensor(),
+        T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (256, 256))
+    image = transform(image).unsqueeze(0)
+    return image.numpy()
+
+# Init Cam
+stream = VideoStream().start()
 
 started = time.time()
 last_logged = time.time()
@@ -30,13 +41,45 @@ frame_count = 0
 thres = 0.56
 
 while True:
-    classIds, confs, bbox = net.detect(stream.read(), confThreshold=thres)
+    # Capture image
+    frame = stream.read()
+
+    # Detect object
+    classIds, confs, bbox = net.detect(frame, confThreshold=thres)
+    
+## Measure distance using MiDaS ##
+
+    input_image = transform_image(frame)
+    ort_inputs = {ort_session.get_inputs()[0].name: input_image}
+    ort_outs = ort_session.run(None, ort_inputs)
+    depth_map = ort_outs[0].squeeze()
+
+    # Resize depth map to match frame size
+    depth_map = cv2.resize(depth_map, (frame.shape[1], frame.shape[0]))
+
+    # # Normalize depth map for visualization
+    # depth_map_normalized = cv2.normalize(depth_map, None, 0, 255, cv2.NORM_MINMAX)
+    # depth_map_colored = cv2.applyColorMap(depth_map_normalized.astype(np.uint8), cv2.COLORMAP_JET)
+
+    # # Blend the depth map with the original frame
+    # blended_frame = cv2.addWeighted(frame, 0.6, depth_map_colored, 0.4, 0)
+
+## Measure Distance end ##
+
+    # Output
     if len(classIds) != 0:
         for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
             label = f'{labels[classId - 1]}: {confidence:.2f}'
             print(f"Detected {label}")
             # cv2.rectangle(frame, box, color=(0, 255, 0), thickness=2)
             # cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            # Calculate the average depth value within the bounding box
+            x, y, w, h = box
+            depth_values = depth_map[y:y+h, x:x+w]
+            avg_depth = np.mean(depth_values)
+            print(f"Object: {labels[classId - 1]}, Confidence: {confidence:.2f}, Avg Depth: {avg_depth:.2f}")
+
 
     # cv2.imshow('frame', frame)
     
