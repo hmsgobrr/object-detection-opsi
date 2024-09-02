cb092df80d4ebbaefbbcea35422b2e023fe06bd0
hmsgobrr <metalcorehms@gmail.com>
2024-09-02 19:47:06 +0700

    tts test

---
+++ ttstest.py
@@ -0,0 +1,35 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say("One person, near, in front of you")
+engine.runAndWait()
+input()
+
+engine.say("Two cars, far, on your right")
+engine.runAndWait()
+input()
+
+engine.say("Three chairs, near, on your left")
+engine.runAndWait()
+input()
+
+engine.say("Four tables, 114 meters, in front of you")
+engine.runAndWait()
+input()
+
+engine.say("Five bottles, 140 meters, on your left")
+engine.runAndWait()
+input()
+
+engine.say("Six laptops, 278 meters, on your right")
+engine.runAndWait()
+input()
+
+engine.say("One laptop and two persons, far, in front of you")
+engine.runAndWait()
+input()
+
+engine.say("Three bottles, three cars, two tables, near, in front of you")
+engine.runAndWait()
+input()
