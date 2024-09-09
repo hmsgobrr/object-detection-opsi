c8790c48a0d4453322d07855bb67b092e7b56180
hmsgobrr <metalcorehms@gmail.com>
2024-09-09 19:50:18 +0700

    array eror

--- singcamdet-video.py
+++ singcamdet-video.py
@@ -87,9 +87,9 @@ while run and video.isOpened():
     startdet = time.time()
     
     # Split the fraem
-    top_frame = frame[:frame_height // 2, :]
+    top_frame = frame[:frame_width // 2, :]
     top_frame = cv2.resize(top_frame, (320, 160), interpolation=cv2.INTER_NEAREST)
-    bottom_frame = frame[frame_height // 2:, :]
+    bottom_frame = frame[frame_width // 2:, :]
     bottom_frame = cv2.resize(bottom_frame, (320, 160), interpolation=cv2.INTER_NEAREST)
     combined_frame = cv2.vconcat([top_frame, bottom_frame])
 
