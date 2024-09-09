72adce853e0ed9848b208fdc10a058da6af51009
hmsgobrr <metalcorehms@gmail.com>
2024-09-09 19:58:59 +0700

    skibidi

--- singcamdet-video.py
+++ singcamdet-video.py
@@ -88,9 +88,9 @@ while run and video.isOpened():
     startdet = time.time()
     
     # Split the fraem
-    top_frame = frame[:frame_width // 2, :]
+    top_frame = frame[:, :frame_width // 2]
     top_frame = cv2.resize(top_frame, (320, 160), interpolation=cv2.INTER_NEAREST)
-    bottom_frame = frame[frame_width // 2:, :]
+    bottom_frame = frame[:, frame_width//2:]
     bottom_frame = cv2.resize(bottom_frame, (320, 160), interpolation=cv2.INTER_NEAREST)
     combined_frame = np.vstack((top_frame, bottom_frame))
 
